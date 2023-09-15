7dd172effa5270824eaf46084b53615e496ff532
Christopher Angelo <angelo@angeloanan.xyz>
2023-09-16 02:22:42 +0700

    test: fix test to adapt to new rules

--- samples/users.tsx
+++ samples/users.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
+import * as React from 'react';
 
-export default Users
+export default Users;
 
 type Props = {
   nameFilter: string
@@ -8,7 +8,7 @@ type Props = {
 function Users({nameFilter}: Props) {
   const users = [{name: 'Janice', friends: ['Albert', 'Nancy'], id: 23}].filter(
     ({name}) => name.toLowerCase().includes(nameFilter),
-  )
+  );
 
   return (
     <div className="user-list" id="users-section" data-testid="users">
@@ -18,15 +18,15 @@ function Users({nameFilter}: Props) {
         </div>
       ))}
     </div>
-  )
+  );
 }
 
 export function variableNames() {
-  const user$ = 'user stream'
-  const $el = 'dom node'
-  const camelCase = 'camelCase'
-  const PascalCase = 'PascalCase'
-  const __thing = '__filename'
-  const CONST_VALUE = 'CONST_VALUE'
-  return {$el, user$, camelCase, PascalCase, __thing, CONST_VALUE}
+  const user$ = 'user stream';
+  const $el = 'dom node';
+  const camelCase = 'camelCase';
+  const PascalCase = 'PascalCase';
+  const __thing = '__filename';
+  const CONST_VALUE = 'CONST_VALUE';
+  return {$el, user$, camelCase, PascalCase, __thing, CONST_VALUE};
 }
